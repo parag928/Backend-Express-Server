3968e8395fa2ff89a47488db07757417178ace9f
Parag Mayani <paragmayani.28@gmail.com>
2021-07-20 15:41:33 -0400

    Passport

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var FileStore = require('session-file-store')(session);
 
 const mongoose1 = require('mongoose');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
@@ -45,26 +48,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
       next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
   }
 }
 
