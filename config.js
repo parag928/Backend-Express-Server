43208d9ab3173f3a31fa0ce54c1391690cf9720d
Parag Mayani <paragmayani.28@gmail.com>
2021-07-29 16:14:32 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -2,3 +2,11 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion'
 }
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '4781435618536754',
+        clientSecret: '807d585064ac2c0a4cac1f3780bc4cbf'
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,15 @@ router.get('/', cors.corsWithOptions, function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
 router.post('/signup', cors.corsWithOptions, (req, res, next) => {
 
   User.register(new User({username: req.body.username}), 
